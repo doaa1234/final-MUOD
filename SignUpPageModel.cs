18100599f9864a3018ce1f3845dd24863ce31787
Doaa-Eid <78642805+doaa1234@users.noreply.github.com>
2022-04-05 07:34:48 +0300

    Add files via upload

---
+++ SignUpPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppProjectMVVM.Models
+{
+    public class SignUpPageModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ SignUpPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppProjectMVVM.Models;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using AppProjectMVVM.Services;
+using Firebase.Database;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using MvvmHelpers;
+
+namespace AppProjectMVVM.ViewModels
+{
+    public class SignUpPageViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        private DBFirebase Services;
+        public Command AddUserCommand { get; }
+        private ObservableCollection<SignUpPageModel> _users = new ObservableCollection<SignUpPageModel>();
+
+     
+
+        public ObservableCollection<SignUpPageModel> users
+        {
+            get { return _users; }
+            set
+            {
+                _users = value;
+                OnPropertyChanged();
+            }
+        }
+        public SignUpPageViewModel()
+        {
+            Services = new DBFirebase();
+            users = Services.getPerson();
+            AddUserCommand = new Command(async () =>
+              await AddUserAsync(Name, Email, Password));
+        }
+
+        private async Task AddUserAsync(string name, string email, string password)
+        {
+           await Services.Addperson(name, email, password);
+        }
+    }
+}
