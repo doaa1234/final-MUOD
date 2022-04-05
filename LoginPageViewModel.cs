0ac2e83efc348b8fd5ac7cfaa227d9994ad1d76e
Doaa-Eid <78642805+doaa1234@users.noreply.github.com>
2022-04-05 07:32:27 +0300

    Add files via upload

---
+++ LoginPageViewModel.cs
@@ -0,0 +1,213 @@
+﻿using AppProjectMVVM.Services;
+using AppProjectMVVM.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppProjectMVVM.ViewModels
+{
+    public class LoginPageViewModel : BaseViewModel
+    {
+        private string _Useremail;
+        public string Useremail
+        {
+            get
+            {
+                return this._Useremail;
+            }
+            set
+            {
+                this._Useremail = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        private string _Password;
+        public string Password
+        {  
+            get
+            {
+                return this._Password;
+            }
+            set
+            {
+                this._Password = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        private bool _Result;
+        public bool Result
+        {
+            set
+            {
+                this._IsBusy = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return this._IsBusy;
+            }
+        }
+        private bool _IsBusy;
+        public bool IsBusy
+        {
+            set
+            {
+                this._Result = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return this._Result;
+            }
+        }
+
+
+
+        public Command LoginCommand { get; set; }
+        public Command SignUpCommand { get; set; }
+
+        public LoginPageViewModel()
+        {
+            LoginCommand = new Command(async () => await LoginCommandAsync());
+            SignUpCommand = new Command(async () => await RegisterCommandAsync());
+        }
+
+
+
+        private async Task RegisterCommandAsync()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                var userService = new UserService();
+                Result = await userService.RegisterUser(Useremail, Password);
+                if (Result)
+                    await Application.Current.MainPage.DisplayAlert("Success", "Registered User", "Ok");
+                else
+                    await Application.Current.MainPage.DisplayAlert("Error", "Failed to register user", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+
+
+
+
+
+
+        private async Task LoginCommandAsync()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                var userService = new UserService();
+                Result = await userService.LoginUser(Useremail, Password);
+                if (Result)
+                {
+                    Preferences.Set("Useremail", Useremail);
+                    await Application.Current.MainPage.Navigation.PushAsync(new MinPageView());
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Invalid Email/Password(s)", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+        /* private string _errorMessage;
+         private Color _PageColor;
+
+         public event PropertyChangedEventHandler PropertyChanged;
+
+         public string ErrorMessage
+         {
+             get { return this._errorMessage; }
+             set
+             {
+                 this._errorMessage = value;
+                 PropertyChanged(this, new PropertyChangedEventArgs("ErrorMessage"));
+             }
+         }
+         public Color PageColor
+         {
+             get { return _PageColor; }
+             set {
+                 _PageColor = value;
+                 PropertyChanged(this, new PropertyChangedEventArgs("PageColor"));
+
+             }
+
+         }
+         public string Email { get; set; }
+         public string Password { get; set; }
+
+         public ICommand LoginCommand { get; set; }
+         public LoginPageViewModel()
+         {
+             LoginCommand = new Command(() => Login());
+         }
+
+         private void Login()
+         {
+             if(string.IsNullOrEmpty(Email))
+             {
+                 ErrorMessage = "Email is Empty";
+                 PageColor = Color.LightPink;
+             }
+             else if(string.IsNullOrEmpty(Password))
+             {
+                 ErrorMessage = "Password is Empty";
+                 PageColor = Color.LightPink;
+
+             }
+             else
+             {
+                 PageColor = Color.LightGreen;
+                 ErrorMessage = String.Empty;
+                 App.Current.MainPage.DisplayAlert("Welcom", "Hey" + Email, "ok");
+
+             }
+
+         }
+        */
+
+
+
+    }
+}
---
+++ Services/DBFirebase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using AppProjectMVVM.Models;
+using Firebase.Database.Query;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace AppProjectMVVM.Services
+{
+    public class DBFirebase 
+    {
+        FirebaseClient person;
+        public DBFirebase()
+        {
+            person = new FirebaseClient("https://projectmuod-default-rtdb.firebaseio.com/");
+
+
+        }
+        public ObservableCollection<SignUpPageModel> getPerson()
+        {
+            var PersonData = person.Child("Users").AsObservable<SignUpPageModel>().AsObservableCollection();
+            return PersonData;
+        }
+        public async Task Addperson(string name, string email, string password)
+        {
+            SignUpPageModel c = new SignUpPageModel() { Name = name, Email = email, Password = password };
+            await person.Child("Users").PostAsync(c);
+        }
+
+      //  public async Task<bool> IsUser(string userEmail, string userPassword)
+    //    {
+      //      var user=(await person.Child("Users")
+     //           .OnceAsync<SignUpPageModel>())
+      //          .Where()
+       // }
+    }
+}
